8d1503f90b25938dd53f8ca9f58252ac2012b311
Arrastia <arrastia@pm.me>
2022-05-09 20:05:09 +0200

    ADD - useInteraction custom hook

---
+++ src/ui/_components/Button/hooks/useInteraction.tsx
@@ -0,0 +1,21 @@
+import { useCallback, useEffect, useRef } from 'react';
+import { InteractionManager } from 'react-native';
+
+export const useInteraction = () => {
+  const interactionHandle = useRef<number>();
+
+  const createInteractionHandle = useCallback(() => {
+    interactionHandle.current = InteractionManager.createInteractionHandle();
+  }, []);
+
+  const removeInteractionHandle = useCallback(() => {
+    if (interactionHandle.current) {
+      InteractionManager.clearInteractionHandle(interactionHandle.current);
+      interactionHandle.current = undefined;
+    }
+  }, []);
+
+  useEffect(() => () => removeInteractionHandle());
+
+  return [createInteractionHandle, removeInteractionHandle, interactionHandle];
+};
